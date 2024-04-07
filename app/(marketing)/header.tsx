3dedb0efb2bebc634c00ca2e02c1cb9f82613956
AmirrezaJM <amirreza.jolani@yahoo.com>
2024-04-07 16:44:48 +0200

    add images and videos to projject also finished the first part of project.

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,35 @@
+import { ClerkLoaded, ClerkLoading, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+const Header = () => {
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl tracking-wide font-extrabold text-green-600">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal" afterSignInUrl="/learn">
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
